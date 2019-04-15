5d4072030e5983f57c4b81ca2008b949816c7bac
Ben Tingey <bntngey@gmail.com>
2019-04-15 11:56:01 -0600

    uploading all my scripts

---
+++ server.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(shiny)
+library(xgboost)
+server <- function(input, output) {
+  newPredict <- reactive({
+    data.frame(
+      Sepal.Length=input$sep.length, 
+      Sepal.Width=input$sep.width,
+      Petal.Length=input$p.length, 
+      Petal.Width=input$p.width)
+  })
+  
+  output$predictions <- DT::renderDataTable({
+    data.frame(
+      iris = c("setosa", "versicolor", "virginica")
+      ,Probs = predict(IrisModel, as.matrix(newPredict()))
+    )
+  })
+  
+  output$predictions<-DT::renderDataTable({
+    label= as.matrix(c("Sepal.Length", "Sepal.Width", "Petal.Length","Petal.Width","Species")  )
+    newPredictout<-xgb.DMatrix(as.matrix(sapply(newPredict(),as.numeric)),label=label)
+    predict(IrisModel,newPredictout) 
+  })
+  output$scatter<-renderPlot({
+    plot(x=iris$Sepal.Length, y=iris$Sepal.Width, 
+         xlab="Sepal Length", ylab="Sepal Width",  main="Sepal Length-Width")
+    points(newPredict(), col="red", pch=19)
+  })
+  output$den1<-renderPlot({
+    d <- density(iris$Sepal.Width)
+    hist(iris$Sepal.Width, breaks=12, prob=TRUE, xlab="Sepal Width", main="Histogram & Density Curve")
+    lines(d, lty=2, col="blue")
+    abline(v=newPredict()$Sepal.Width, col='red')
+  })
+  output$den2<-renderPlot({
+    d <- density(iris$Sepal.Length)
+    hist(iris$Sepal.Length, breaks=12, prob=TRUE, xlab="Sepal Length", main="Histogram & Density Curve")
+    lines(d, lty=2, col="blue")
+    abline(v=newPredict()$Sepal.Length, col='red')
+  })
+  output$den3<-renderPlot({
+    d <- density(iris$Petal.Length)
+    hist(iris$Petal.Length, breaks=12, prob=TRUE, xlab="Petal Length", main="Histogram & Density Curve")
+    lines(d, lty=2, col="blue")
+    abline(v=newPredict()$Petal.Length, col='red')
+  })
+  output$den3<-renderPlot({
+    d <- density(iris$Petal.Width)
+    hist(iris$Petal.Width, breaks=12, prob=TRUE, xlab="Petal Width", main="Histogram & Density Curve")
+    lines(d, lty=2, col="blue")
+    abline(v=newPredict()$Petal.Width, col='red')
+  })
+}
---
+++ ui.R
@@ -0,0 +1,49 @@
+## ui.R ##
+library(shinydashboard)
+
+
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    numericInput(
+      "sep.length"
+      , "Sepal Length:"
+      , 5)
+    ,
+    numericInput(
+      "sep.width"
+      ,'Sepal Width:'
+      ,3)
+    ,
+    numericInput(
+      "p.length"
+      ,'Petal Length:'
+      ,2)
+    ,
+    numericInput(
+      "p.width"
+      ,'Petal Width:'
+      ,.3)
+  ),
+  dashboardBody(
+    fluidRow(
+      box(DT::dataTableOutput("predictions"))
+      # ,box(plotOutput("graph"))
+    ),
+    fluidRow(
+      tabPanel( 'Plot', plotOutput('scatter'))
+    ),
+    fluidRow(
+      tabPanel("Plot", plotOutput("den1"))
+    ),
+    fluidRow(
+      tabPanel("Plot", plotOutput("den2"))
+    ),
+    fluidRow(
+      tabPanel("Plot", plotOutput("den3"))
+    ) ,
+    fluidRow(
+      tabPanel("Plot", plotOutput("den4"))
+    )
+  )
+)
